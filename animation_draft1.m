bb204f4851b759a9e10f8fb096241d092c377339
srli <sophia.li@students.olin.edu>
2015-11-18 10:01:44 -0500

    animation draft

--- animation_draft1.m
+++ animation_draft1.m
@@ -1,12 +1,17 @@
+
+
 % First make the patch ...
 
 figure
-    t = 0:2*pi/20:2*pi; 
-    X = 10 + sin(t); Y = 2 + cos(t); Z = X*0; 
-    h = patch(X,Y,Z,'g') 
+    t1 = 0:2*pi/20:2*pi; 
+    X = 5 + sin(t); Y = 2 + cos(t); Z = X*0; 
+    h1 = patch(X,Y,Z,'g');
+    h2 = patch(X,Y,Z,'b');
     axis([-10 10 -10 10]); 
 % Then make it movable 
-    moveit2(h); 
+    moveit2(h1); 
+    moveit2(h2);
+    thing = h1.XData
   
 %It also works for e.g. pcolor and plot:
 
