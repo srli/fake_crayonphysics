4f5b7a3b7d5a0036c18bf6f834cd7269cedc0888
srli <sophia.li@students.olin.edu>
2015-11-18 09:54:18 -0500

    move it demo

--- untitled.m
+++ untitled.m
@@ -1,17 +1,15 @@
+% First make the patch ...
+
 figure
+    t = 0:2*pi/20:2*pi; 
+    X = 10 + sin(t); Y = 2 + cos(t); Z = X*0; 
+    h = patch(X,Y,Z,'g') 
+    axis([-10 10 -10 10]); 
+% Then make it movable 
+    moveit2(h); 
+  
+%It also works for e.g. pcolor and plot:
 
-load clown
-
-imagesc(X)
-colormap(map)
-axis off
-
-hold on
-
-rectangle('position',[50 30 70 30], ...
-    'facecolor','r')
-
-f=getframe(gca)
-[X, map] = frame2im(f);
-
-imwrite(X,'test.jpg','jpeg')
+% Adjust data to fit hypothesis using the mouse ... :-) 
+%a = plot(rand(10,10)); 
+%moveit2(a);
